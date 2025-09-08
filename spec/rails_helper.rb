0d9d076cb8f93868a177267dbb17fdf2c735a236
Katsuya HIDAKA <hidakatsuya@gmail.com>
2025-09-09 01:33:08 +0900

    add no-sandbox

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
       unless ENV['HEADLESS'] == '0'
         options.add_argument('headless')
         options.add_argument('disable-gpu')
+        options.add_argument('no-sandbox')
       end
       Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
     end
